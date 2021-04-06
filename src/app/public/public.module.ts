c5bd2dab6d0e2ade3419329b57ef26f55cc8e93f
TASYA DIMATAURI <tasya1900016039@webmail.uad.ac.id>
2021-04-06 09:17:50 +0700

    project-1

---
+++ src/app/public/public.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PublicComponent } from './public.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+
+// Material Design
+import { MaterialDesign } from '../material/material';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PublicComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent
+      }
+    ]
+  }
+]
+
+@NgModule({
+  declarations: [PublicComponent, HomeComponent],
+  imports: [
+    CommonModule,
+    MaterialDesign,
+    RouterModule.forChild(routes)
+  ]
+})
+export class PublicModule { }
